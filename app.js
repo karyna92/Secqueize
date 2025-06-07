26e3f2d00882400542d069871b9ab5fcc09fff2f
Karyna Prokipchuk <karynaprokipchuk@gmail.com>
2025-06-07 13:37:23 +0300

    Add env

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const router = require('./routes');
 const errorHandler=require('./errorHandler')
+const { STATIC_PATH } = require('./config/path.configs.js');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   let data = '';
-//   req.on('data', (chunk) => {
-//     data += chunk;
-//   });
-//   req.on('end', () => {
-//     console.log('Raw body:', data);
-//     next();
-//   });
-// });
 
 
 app.use(express.json());
 
+app.use(express.static(STATIC_PATH));
+
 app.use('/api', router);
+
 app.use(errorHandler)
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const basename = path.basename(__filename);
 
 //зчитали конфіги
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 // створили підключення до дб
